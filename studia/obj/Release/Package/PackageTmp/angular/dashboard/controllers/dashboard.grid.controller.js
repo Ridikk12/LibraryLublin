fa40b0bdd62703ebbafaddb2ae2f3ac435fa35a4
Ridikk12 <n.debosz@gmail.com>
2017-02-12 20:37:29 +0100

    Signed-off-by: Ridikk12 <n.debosz@gmail.com>

---
+++ studia/obj/Release/Package/PackageTmp/angular/dashboard/controllers/dashboard.grid.controller.js
@@ -0,0 +1,21 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('libraryModule')
+        .controller('dashboardGird', dashboardGird);
+
+    dashboardGird.$inject = ['$scope'];
+
+    function dashboardGird($scope) {
+
+        $scope.userBooks = {};
+        $scope.init = function(){
+            $.get("Library/UserBooks", function (result) {
+                debugger;
+                $scope.userBooks = result;
+            });
+        }
+       
+    }
+})();
